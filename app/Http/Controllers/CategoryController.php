7e1b69a361d70f105d5824df66f865bb454bcec2
pedro <pedrollongo@gmail.com>
2019-06-05 14:02:26 +0200

    Guardar una categoría

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -8,9 +8,17 @@ use Illuminate\Http\Request;
 class CategoryController extends Controller
 {
 
-    //|        | GET|HEAD  | api/category                 | category.index   | App\Http\Controllers\CategoryController@index   | web                                       |
     /**
-     * @return \Illuminate\Http\JsonResponse
+     * CategoryController constructor.
+     * Cargamos el middleware auth excepto para metodos...
+     */
+    public function __construct() {
+        $this->middleware('\App\Http\Middleware\ApiAuthMiddleware', ['except' => ['index', 'show']]);
+    }
+
+    /**
+     * |        | GET|HEAD  | api/category                 | category.index   | App\Http\Controllers\CategoryController@index   | web                                       |
+ * @return \Illuminate\Http\JsonResponse
      */
     public function index() {
         $categories = Category::all();
@@ -22,8 +30,44 @@ class CategoryController extends Controller
         return response()->json($data, $data['code']);
     }
 
-    //|        | POST      | api/category                 | category.store   | App\Http\Controllers\CategoryController@store   | web                                       |
-    public function store($category) {
+
+    /**
+     * |        | POST      | api/category                 | category.store   | App\Http\Controllers\CategoryController@store   | web                                       |
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request) {
+
+        // recoger datos por post
+        $json = $request->input('json', null);
+        $params_array = json_decode($json, true);
+
+        // validar datos
+        $validate = \Validator::make($params_array, [
+            'name' => 'required'
+        ]);
+        // guardar categoria
+        if ($validate->fails() || isEmpty($params_array)) {
+            $data = [
+                'status'  => 'error',
+                'code'    => 400,
+                'message' => 'La categoria no se ha guardado',
+            ];
+            if (isEmpty($params_array)) $data['message'] = 'No has enviado ningun nombre de categoria';
+        } else {
+            $category = new Category();
+            $category->name = $params_array['name'];
+            $category->save();
+
+            $data = [
+                'status'   => 'success',
+                'code'     => 200,
+                'category' => $category,
+            ];
+        }
+
+        // devolver resultado
+        return response()->json($data, $data['code']);
 
     }
 
@@ -33,8 +77,8 @@ class CategoryController extends Controller
     //|        | DELETE    | api/category/{category}      | category.destroy | App\Http\Controllers\CategoryController@destroy | web                                       |
     //|        | PUT|PATCH | api/category/{category}      | category.update  | App\Http\Controllers\CategoryController@update  | web                                       |
 
-    //|        | GET|HEAD  | api/category/{category}      | category.show    | App\Http\Controllers\CategoryController@show    | web                                       |
     /**
+     * |        | GET|HEAD  | api/category/{category}      | category.show    | App\Http\Controllers\CategoryController@show    | web                                       |
      * @param $id
      * @return \Illuminate\Http\JsonResponse
      */
